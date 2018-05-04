a0dfa5bb0833b52b1115994e8683c1ae453373e0
harrixon <harrisonchan1991@gmail.com>
2018-05-04 16:54:39 +0800

    move init session to separate js

--- app.js
+++ app.js
@@ -10,34 +10,9 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 // redis
-// init redis connection
-const redis = require("redis");
-const redisClient = redis.createClient({
-    host: "localhost",
-    port: 6379
-});
-redisClient.on("error", function (err) {
-    console.log(`REDIS: ${err}`);
-});
-
-// init-session
-const expressSession = require('express-session');
-const RedisStore = require('connect-redis')(expressSession);
-const socketIOSession = require("socket.io.session");
-
-const sessionStore = new RedisStore({
-    client: redisClient,
-    unset: "destroy"
-});
-const settings = {
-    store: sessionStore,
-    secret: "supersecret",
-    cookie: { "path": '/', "httpOnly": true, "secure": false, "maxAge": null }
-    // maxAge : 10 * 60 * 1000 ms
-}
-
-app.use(expressSession(settings));
-io.use(socketIOSession(settings).parser);
+const redisClient = require("./init-session").redisClient;
+const initSession = require("./init-session").init;
+initSession(app, io);
 
 // passport 
 const setupPassport = require('./passport');
